2405151d9d2a42fe8e7731e38d365e83314c88a2
flatironschool <t.benson@live.co.uk>
2019-12-16 17:03:21 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,43 @@ class Artist
 
   attr_reader :name, :years_experience
 
-  def initialize(name, years_experience)
+  @@all = []
+
+  def initialize(name:, years_experience:)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
+
+  def paintings
+    Painting.all.select { |painting| painting.artist == self }
+  end
+
+  def galleries
+    paintings.map { |painting| painting.gallery }.uniq
+  end
+
+  def cities
+    galleries.map { |gallery| gallery.city }
+  end
+
+  def create_painting(title:, price:, gallery:)
+    Painting.new(title: title, price: price, gallery: gallery, artist: self)
+  end
+
+# class methods
+
+  def self.all
+    @@all
+  end
+
+  def self.total_experience
+    self.all.map { |artist| artist.years_experience }.reduce(:+)
+  end
+
+  def self.most_prolific
+    self.all.max_by { |artist| (artist.paintings.count / artist.years_experience.to_f) }
+  end
+
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,27 @@
 class Painting
 
   attr_reader :title, :price
+  attr_accessor :artist, :gallery
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title:, price:, artist:, gallery:)
+    @artist = artist
+    @gallery = gallery
     @title = title
     @price = price
+    @@all << self
+  end
+
+
+# class methods:
+
+  def self.all
+    @@all
+  end
+
+  def self.total_price
+    self.all.map { |painting| painting.price }.reduce(:+)
   end
 
 end
